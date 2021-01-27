a4afea22244b6982e39781a1d406fea2afe77b43
Sulami <SulamiWizard@gmail.com>
2021-01-26 22:09:25 -0800

    idk what to put here yet

---
+++ src/main/java/com/sulamiwizard/sulamiaddons/SulamiAddons.java
@@ -0,0 +1,30 @@
+package com.sulamiwizard.sulamiaddons;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = SulamiAddons.MODID, version = SulamiAddons.VERSION, name = SulamiAddons.NAME)
+public class SulamiAddons {
+    public static final String MODID = "sulamiaddons";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "SulamiAddons";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
