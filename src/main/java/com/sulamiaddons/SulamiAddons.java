3cdfd693ed4eec77dfcaa1668d5a3edbb5b9c601
Sulaiman Waziri <SulamiWizard@gmail.com>
2023-07-13 20:57:00 -0700

    -Removed Refined Adamantite, -Added Block of Adamantite -Added custom item group called SulamiAddons -Added raw and ingot forms of adamantite to the ingredients group

--- src/main/java/com/sulamiaddons/SulamiAddons.java
+++ src/main/java/com/sulamiaddons/SulamiAddons.java
@@ -1,25 +1,26 @@
 package com.sulamiaddons;
 
 import com.sulamiaddons.block.ModBlocks;
+import com.sulamiaddons.item.ModItemGroups;
 import com.sulamiaddons.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SulamiAddons implements ModInitializer {
     public static final String MOD_ID = "sulamiaddons";
+
     // This logger is used to write text to the console and the log file.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
 
         LOGGER.info("Hello Fabric world!");
+        ModItemGroups.registerItemGroups();
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
+
     }
 }
